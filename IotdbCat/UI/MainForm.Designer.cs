92aed72b1e485f0d7b99b9fea6426aa556900ea1
haoyuechen <1142367616@qq.com>
2021-04-23 09:05:47 +0800

    change gui

--- IotdbCat/UI/MainForm.Designer.cs
+++ IotdbCat/UI/MainForm.Designer.cs
@@ -29,10 +29,17 @@ namespace IotdbCat.UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1122, 685);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
