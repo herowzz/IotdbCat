f3fd43fd84d5f77c5807fd9c4535e513e9b16986
wangzz <wzzwxh@gmail.com>
2021-04-23 09:00:33 +0800

    add DbDao, CodeTip, DateUtils

---
+++ IotdbCat/Dao/DbDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotdbCat.Dao
+{
+    /// <summary>
+    /// Db data access object
+    /// </summary>
+    public class DbDao
+    {
+    }
+}
---
+++ IotdbCat/Domain/CodeTip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotdbCat.Domain
+{
+    /// <summary>
+    /// code pop tips
+    /// </summary>
+    public class CodeTip
+    {
+        public string Name { get; set; }
+
+        public TipType Type { get; set; }
+    }
+
+    public enum TipType
+    {
+        Syntax, Table, Function
+    }
+}
